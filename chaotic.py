fc72be01440d829781d9f4222b424b34fd9f83e7
ARZ3N <aniZ3n99@outlook.com>
2021-07-17 19:19:35 +0530

    dice roll def-

--- chaotic.py
+++ chaotic.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 vortices = input("Enter number of Vortices -> ")
 Vort_x = np.zeros(int(vortices))
@@ -11,4 +12,10 @@ for i in range(1, 4):
     np.append(Vort_x, x)
     np.append(Vort_y, y)
 
-  
+init_x = input("Enter X crdnt of strtng pnt -> ")
+init_y = input("Enter Y crdnt of strtng pnt -> ")
+
+# a <= rand_int <= b : randrange(a, b+1)
+def roll_dice(n_vortice):
+    return random.randrange(1, n_vortice+1)
+
