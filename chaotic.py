99162937196cdc6184f34b8dff350a007c275418
ARZ3N <aniZ3n99@outlook.com>
2021-07-17 18:35:25 +0530

    inputs-

--- chaotic.py
+++ chaotic.py
@@ -1,3 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-print("....Ignition.......and Lift off!!....")
+
+vortices = input("Enter number of Vortices -> ")
+Vort_x = np.zeros(int(vortices))
+Vort_y = np.zeros(int(vortices))
+
+for i in range(1, 4):
+    x = input("Enter "+ i +"vortice X crdnt: ")
+    y = input("Enter "+ i +"vortice Y crdnt: ")
+    np.append(Vort_x, x)
+    np.append(Vort_y, y)
+
+  
